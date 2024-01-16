f32b7764643ef325f9c163c20d1f2ce1afb3eead
valmon55 <fedor_ka@mail.ru>
2024-01-16 22:47:22 +0300

    + Logger

---
+++ ASP/ASP.AuthenticationService/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ASP.AuthenticationService
+{
+    public class Logger
+    {
+        public void WriteEvent(string eventMessage)
+        {
+            Console.WriteLine(eventMessage);
+        }
+        public void WriteError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
