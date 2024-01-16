6afe88e6347d349decc5fe91debad6a57dbce37f
valmon55 <fedor_ka@mail.ru>
2024-01-16 23:19:43 +0300

    + GetUser

--- ASP/ASP.AuthenticationService/Controllers/UserController.cs
+++ ASP/ASP.AuthenticationService/Controllers/UserController.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ASP.AuthenticationService.Controllers
 {
-    public class UserController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
     {
-
+        public UserController() 
+        {
+            var logger = new Logger();
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+        }
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Fedor",
+                LastName = "Kr",
+                Login = "fedor",
+                Password = "1112233qw",
+                Email = "fedor@mail.ru"
+            };
+        }
     }
 }
