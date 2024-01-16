15a86a45eff0bd7cd81a37b7050eddd0583a2341
valmon55 <fedor_ka@mail.ru>
2024-01-16 23:03:27 +0300

    + UserController

---
+++ ASP/ASP.AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.AuthenticationService.Controllers
+{
+    public class UserController : Controller
+    {
+
+    }
+}
