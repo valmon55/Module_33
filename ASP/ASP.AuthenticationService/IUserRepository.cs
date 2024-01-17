083dcee56fb161ca191db588002e0e8bb2a2a734
valmon55 <fedor_ka@mail.ru>
2024-01-17 12:53:15 +0300

    unit 3 start

---
+++ ASP/ASP.AuthenticationService/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ASP.AuthenticationService
+{
+    public interface IUserRepository
+    {
+        public IEnumerable<User> GetAll();
+        public User GetByLogin(string login);
+    }
+}
---
+++ ASP/ASP.AuthenticationService/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ASP.AuthenticationService
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetByLogin(string login)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
