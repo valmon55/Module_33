ce68aea13ed42094217a51e7d040274c7136980f
valmon55 <fedor_ka@mail.ru>
2024-01-17 11:30:02 +0300

    unit 2 done

--- ASP/ASP.AuthenticationService/Logger.cs
+++ ASP/ASP.AuthenticationService/Logger.cs
@@ -11,15 +11,22 @@ namespace ASP.AuthenticationService
         FileInfo errorFile;
         public Logger() 
         {
-            logDirectory = new DirectoryInfo("Log" + DateTime.Now.ToString());
+            logDirectory = new DirectoryInfo("Log_" + DateTime.Now.Day.ToString() + "_" +
+                                                      DateTime.Now.Month.ToString() + "_" +
+                                                      DateTime.Now.Year.ToString() + "_" +
+                                                      DateTime.Now.Hour.ToString() + "_" +
+                                                      DateTime.Now.Minute.ToString() + "_" +
+                                                      DateTime.Now.Second.ToString() + "_" +
+                                                      DateTime.Now.Millisecond.ToString()
+                );
             if (!logDirectory.Exists)
             {
                 logDirectory.Create();
             }
-            eventFile = new FileInfo("event.txt");
-            errorFile = new FileInfo("error.txt");
+            eventFile = new FileInfo($"{logDirectory}\\event.txt");
+            errorFile = new FileInfo($"{logDirectory}\\error.txt");
         }
-        public async Task WriteEvent(string eventMessage)
+        public async void WriteEvent(string eventMessage)
         {
             //Console.WriteLine(eventMessage);
             using(var sw = eventFile.CreateText())
@@ -27,9 +34,13 @@ namespace ASP.AuthenticationService
                 await sw.WriteLineAsync(eventMessage);
             }
         }
-        public void WriteError(string errorMessage)
+        public async void WriteError(string errorMessage)
         {
-            Console.WriteLine(errorMessage);
+            //Console.WriteLine(errorMessage);
+            using(var sw = errorFile.CreateText()) 
+            { 
+                await sw.WriteLineAsync(errorMessage);
+            }
         }
     }
 }
