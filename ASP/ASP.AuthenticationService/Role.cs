8c0bc31e977213d72457a48c1d61c671c65fa700
valmon55 <fedor_ka@mail.ru>
2024-01-17 16:28:52 +0300

    вопрос решен

--- ASP/ASP.AuthenticationService/Role.cs
+++ ASP/ASP.AuthenticationService/Role.cs
@@ -1,6 +1,10 @@
-﻿namespace ASP.AuthenticationService
+﻿using System;
+
+namespace ASP.AuthenticationService
 {
     public class Role
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- ASP/ASP.AuthenticationService/UserRepository.cs
+++ ASP/ASP.AuthenticationService/UserRepository.cs
@@ -1,17 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ASP.AuthenticationService
 {
     public class UserRepository : IUserRepository
     {
+        private readonly List<User> _users = new List<User>();
+        public UserRepository() 
+        {
+            _users.Add(new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "11111122222qq",
+                Login = "ivanov",
+                Role = new Role() { Id = 1, Name = "Пользователь"}
+            });
+
+            _users.Add(new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Максим",
+                LastName = "Максимов",
+                Email = "maksim@gmail.com",
+                Password = "11",
+                Login = "maxim",
+                Role = new Role() { Id = 2, Name = "Администратор" }
+            });
+
+            _users.Add(new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Антон",
+                LastName = "Антонов",
+                Email = "anton@gmail.com",
+                Password = "111zzxc1",
+                Login = "anton",
+                Role = new Role() { Id = 1, Name = "Пользователь" }
+            });
+        }
         public IEnumerable<User> GetAll()
         {
-            throw new System.NotImplementedException();
+            return _users;
         }
 
         public User GetByLogin(string login)
         {
-            throw new System.NotImplementedException();
+            return _users.FirstOrDefault(v => v.Login == login);
         }
     }
 }
